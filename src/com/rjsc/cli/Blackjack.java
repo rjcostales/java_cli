4291c15502ca28000806fed8f94207c53c5e720b
Jesse Costales <jcostales@ean.com>
2016-05-05 12:37:26 -0500

    Add DateValidator

--- src/com/rjsc/cli/Blackjack.java
+++ src/com/rjsc/cli/Blackjack.java
@@ -1,3 +1,5 @@
+package com.rjsc.cli;
+
 import java.util.Enumeration;
 import java.util.Random;
 import java.util.Vector;
---
+++ src/com/rjsc/cli/Fibonacci.java
@@ -0,0 +1,47 @@
+package com.rjsc.cli;
+
+import java.util.Date;
+
+public class Fibonacci {
+
+    public static void main(String argv[]) {
+
+        Date start = new Date();
+        long n = fibonacci(93);
+        Date end = new Date();
+
+        System.out.println(n);
+        System.out.println(end.getTime() - start.getTime());
+        System.out.println(start.toString());
+        System.out.println(end.toString());
+    }
+
+    static  long fibonacci(int n) {
+        switch (n) {
+            case 0:
+                return 0;
+            case 1:
+                return 1;
+            case 2:
+                return 1;
+            case 3:
+                return 2;
+            case 4:
+                return 3;
+            case 5:
+                return 5;
+            case 6:
+                return 8;
+            case 7:
+                return 13;
+            case 8:
+                return 21;
+            case 9:
+                return 34;
+            case 10:
+                return 55;
+            default:
+                return 55 * fibonacci(n - 9) + 34 * fibonacci(n - 10);
+        }
+    }
+}
